a5ba27e7db3e65a7a3aaa48c0f55904b7c5dfa10
onanahauwa <onanahauwa@gmail.com>
2025-02-17 10:32:19 +0100

    Removed the render URL, Added a new path to urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path("", views.HomeView, name="home"),
     path('home/', views.HomeView, name='index'),
     path("accounts/", include("django.contrib.auth.urls")),
     path("logout/", views.logout_view, name="logout"),
